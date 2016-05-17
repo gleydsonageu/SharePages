ba810e4e2183bfd2dfa2180160e4bad386f47fe1
Gleydson <gleydsonageu@gmail.com>
2016-05-17 02:51:51 -0300

    commit da activity tela_inicial. confirmando!

--- app/src/main/java/br/com/projetoapp/sharepages/gui/Tela_inicial.java
+++ app/src/main/java/br/com/projetoapp/sharepages/gui/Tela_inicial.java
@@ -2,8 +2,6 @@ package br.com.projetoapp.sharepages.gui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import br.com.projetoapp.sharepages.R;
 
@@ -13,27 +11,5 @@ public class Tela_inicial extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tela_inicial, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
