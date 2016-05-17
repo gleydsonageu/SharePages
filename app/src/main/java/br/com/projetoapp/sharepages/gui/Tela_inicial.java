751c6d91b17624d5015e025e8dd1d3b9a3fd3789
gleydsonageu <gleydsonageu@gmail.com>
2016-05-16 21:06:33 -0300

    comite telas iniciais

--- app/src/main/java/br/com/projetoapp/sharepages/gui/Tela_inicial.java
+++ app/src/main/java/br/com/projetoapp/sharepages/gui/Tela_inicial.java
@@ -1,22 +1,24 @@
-package br.com.projetoapp.sharepages;
+package br.com.projetoapp.sharepages.gui;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import br.com.projetoapp.sharepages.R;
+
+public class Tela_inicial extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_inicial);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_tela_inicial, menu);
         return true;
     }
 
