af6cb37e55386d5c231e924aa947e2b69021d4f2
Clarissa <c_souza05@hotmail.com>
2016-06-01 19:30:40 -0300

    Tela de cadastro de livro. A outra foi criada errada.

--- app/src/main/java/br/com/projetoapp/sharepages/gui/CadastroLivro.java
+++ app/src/main/java/br/com/projetoapp/sharepages/gui/CadastroLivro.java
@@ -1,7 +1,15 @@
 package br.com.projetoapp.sharepages.gui;
 
-
 import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.projetoapp.sharepages.R;
 
 public class CadastroLivro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_livro);
+    }
 }
