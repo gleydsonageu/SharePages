593683522c2d90baacf299fd89b3ce92f9d42b01
Gleydson <gleydsonageu@gmail.com>
2016-06-10 05:16:22 -0300

    chamando autor e tema na listaLivro

--- app/src/main/java/br/com/projetoapp/sharepages/infra/AdapterListLivro.java
+++ app/src/main/java/br/com/projetoapp/sharepages/infra/AdapterListLivro.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import br.com.projetoapp.sharepages.R;
 import br.com.projetoapp.sharepages.dominio.Livro;
+import br.com.projetoapp.sharepages.dominio.Tema;
 import br.com.projetoapp.sharepages.dominio.UnidadeLivro;
 
 public class AdapterListLivro extends BaseAdapter {
@@ -42,10 +43,14 @@ public class AdapterListLivro extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         UnidadeLivro item = itens.get(position);
         Livro livro = item.getLivro();
+        String autor = item.getLivro().getAutor();
+        Tema tema = item.getLivro().getTema();
 
         convertView = inflater.inflate(R.layout.item_lista_livro, null);
 
         ((TextView) convertView.findViewById(R.id.text)).setText(livro.getNome());
+        ((TextView) convertView.findViewById(R.id.autor)).setText(autor);
+        ((TextView) convertView.findViewById(R.id.tema)).setText(tema.getNome());
 
         return convertView;
     }
