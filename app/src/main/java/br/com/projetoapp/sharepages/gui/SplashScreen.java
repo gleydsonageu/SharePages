65a96ba1f52a0684212bc6f08250c046fdeed575
Gleydson <gleydsonageu@gmail.com>
2016-06-12 13:53:18 -0300

    Tela SplashScreen

---
+++ app/src/main/java/br/com/projetoapp/sharepages/gui/SplashScreen.java
@@ -0,0 +1,30 @@
+
+package br.com.projetoapp.sharepages.gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import br.com.projetoapp.sharepages.R;
+
+public class SplashScreen extends Activity {
+
+    private final int DURACAO_DA_TELA = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent chamarTelaInicial = new Intent(SplashScreen.this, TelaInicial.class);
+
+                SplashScreen.this.startActivity(chamarTelaInicial);
+                SplashScreen.this.finish();
+            }
+        }, DURACAO_DA_TELA);
+    }
+}
