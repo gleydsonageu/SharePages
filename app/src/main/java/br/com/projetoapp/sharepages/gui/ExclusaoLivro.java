7bf93daa271f572e9ad116e0bec6423cae6c9fee
Clarissa <c_souza05@hotmail.com>
2016-06-19 17:22:03 -0300

    Icones agora no projeto (commit anterior foi sem as fotos)

---
+++ app/src/main/java/br/com/projetoapp/sharepages/gui/ExclusaoLivro.java
@@ -0,0 +1,15 @@
+package br.com.projetoapp.sharepages.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.projetoapp.sharepages.R;
+
+public class ExclusaoLivro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exclusao_livro);
+    }
+}
